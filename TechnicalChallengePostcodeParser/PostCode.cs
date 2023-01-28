362b6748f4de47d1b231960769c3ae9de087b9fa
Oliver Bourne <otgb97@gmail.com>
2023-01-28 11:18:51 +0000

    initial idea

---
+++ TechnicalChallengePostcodeParser/PostCode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalChallengePostcodeParser;
+
+public class PostCode
+{
+    public string OutwardCode { get; init; }
+
+    public string OutwardLetter { get; init; }
+
+    public string OutwardNumber { get; set; }
+
+    public string InwardCode { get; init; }
+
+    private PostCode(string outwardCode, string outwardLetter, string outwardNumber, string inwardCode)
+    {
+        OutwardCode = outwardCode;
+        OutwardLetter = outwardLetter;
+        OutwardNumber = outwardNumber;
+        InwardCode = inwardCode;
+    } 
+
+    public static PostCode FromString (string postCode)
+    {
+        postCode = postCode.Replace(" ", "").ToUpper();
+
+        if (postCode.Length < 5 || postCode.Length > 7)
+        {
+            throw new ArgumentException("Invalid post code, need to between 5 and 7 characters");
+        }
+        
+        string outwardCode = postCode.Substring(0, postCode.Length - 3);
+        string inwardCode = postCode.Substring(postCode.Length - 3, 3);
+
+        string outwardLetter = "";
+        string outwardNumber = "";
+
+        return new PostCode(outwardCode, outwardLetter, outwardNumber, inwardCode);
+    }
+}
---
+++ TechnicalChallengePostcodeParser/Program.cs
@@ -0,0 +1,22 @@
+﻿using TechnicalChallengePostcodeParser;
+
+List<String> postCodes = new List<String>
+{
+    "M28 7JP",
+    "WC2H7DE",
+    "CT21 4LR",
+    "N33DP"
+};
+
+foreach(string postcode in postCodes)
+{
+    PostCode parsedPostCode = PostCode.FromString(postcode);
+
+    Console.WriteLine($"# POSTCODE: {postcode}");
+    Console.WriteLine($"\tOUTWARD CODE: {parsedPostCode.OutwardCode}");
+    Console.WriteLine($"\t\tOUTWARD LETTER: {""}");
+    Console.WriteLine($"\t\tOUTWARD NUMBER: {""}");
+    Console.WriteLine($"\tINWARD CODE: {parsedPostCode.InwardCode}");
+    Console.WriteLine();
+    Console.WriteLine();
+}
